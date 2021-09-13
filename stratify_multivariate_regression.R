02c52268c13c636d162fa023d32d5b1c613046cd
DhrubaDR <dasroy.dhruba@gmail.com>
2021-09-13 13:32:37 +0530

    commit_3

--- stratify_multivariate_regression.R
+++ stratify_multivariate_regression.R
@@ -33,4 +33,32 @@ dat %>% ggplot(aes(HR_per_game, R_per_game)) +
 # slope of regression line after stratifying by BB
 dat %>%  
   group_by(BB_strata) %>%
-  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game))
+
+
+
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+
+# Apply the lm() function.
+relation <- lm(y~x)
+
+print(summary(relation))
+
+
+# Create the predictor and response variable.
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+relation <- lm(y~x)
+
+# Give the chart file a name.
+png(file = "linearregression.png")
+
+# Plot the chart.
+plot(y,x,col = "blue",main = "Height & Weight Regression",
+     abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
+
+# Save the file.
+dev.off()
+
+#Logistic regression
